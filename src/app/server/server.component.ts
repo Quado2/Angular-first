59ae87f3a130c899577a4db8d5c6daa1ee0844ef
Quado2 <engchikwas404@gmail.com>
2022-05-17 10:17:44 +0100

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from "@angular/core";
   templateUrl:"./server.component.html"
 })
 export class ServerComponent {
+  serverName = "Quado Server";
+  serverId = "23j2e"
 
+  getServerName(){
+    return this.serverName
+  }
 }
