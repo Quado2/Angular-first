7441ba29c478e3695581709c2b212dae9643edc1
Quado2 <engchikwas404@gmail.com>
2022-05-17 12:48:20 +0100

    ngstyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,23 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:"app-server",
-  templateUrl:"./server.component.html"
+  templateUrl:"./server.component.html",
+  styleUrls:["./server.component.scss"]
 })
 export class ServerComponent {
   serverName = "Quado Server";
-  serverId = "23j2e"
+  serverId = "23j2e";
+  serverStatus: string;
+
+  constructor(){
+    Math.random() > 0.5 ? this.serverStatus = "online": this.serverStatus="offline"
+  }
 
   getServerName(){
     return this.serverName
+  }
+
+  getColor(){
+    return this.serverStatus == "online" ? "green": "red"
   }
 }
